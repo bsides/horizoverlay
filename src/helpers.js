5fd37327423149e8bd4cb63df53f9578c61988fc
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-10 02:35:03 -0300

    Helpers to make life easier

---
+++ src/helpers.js
@@ -0,0 +1,26 @@
+export function getRandom(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export const jobsTank = ['drk', 'gla', 'mrd', 'pld', 'war']
+export const JobsHealer = ['ast', 'cnj', 'sch', 'whm']
+export const jobsDps = [
+  'acn',
+  'arc',
+  'blm',
+  'brd',
+  'drg',
+  'lnc',
+  'mch',
+  'mnk',
+  'nin',
+  'pgl',
+  'pug',
+  'rdm',
+  'rog',
+  'sam',
+  'smn',
+  'thm'
+]
