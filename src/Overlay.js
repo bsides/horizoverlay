225f3efb092d9a2bead764e288b4555f7a0507fe
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-10 02:41:26 -0300

    I should commit more often. Lots of stuff:
    * We have default config values
    * If the user didn't have any saved, it save the default ones to localStorage, thus giving us data even if the user closes the browser
    * Form from config saves data (both state and localStorage)
    * Some props being documented for a better future™
    
    TODO:
    * Better check @ values in shouldComponentUpdate method
    * Actually DO SOMETHING with the values stored! Dude! >.>

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import { object, bool } from 'prop-types'
 import Encounter from './Encounter'
 import Combatants from './Combatants'
+import Config from './Config'
 
 class Overlay extends Component {
+  static propTypes = {
+    Combatant: object,
+    Encounter: object,
+    isActive: bool
+  }
   state = {
-    currentViewIndex: 0
+    currentViewIndex: 0,
+    config: {},
+    openConfig: false
+  }
+  defaultConfig = {
+    color: 'default',
+    encounterDuration: false,
+    encounterTotalDps: false
+  }
+  componentDidMount() {
+    const configStore = localStorage.getItem('horizoverlay')
+    if (!configStore) {
+      const config = this.defaultConfig
+      localStorage.setItem('horizoverlay', JSON.stringify(config))
+      this.setState({ config })
+    } else {
+      const config = JSON.parse(configStore)
+      this.setState({ config })
+    }
   }
   shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.Encounter.encdps === '---') {
-      return false
-    }
+    // if (this.state.openConfig) return false
+    if (nextProps.Encounter.encdps === '---') return false
     if (this.state.currentViewIndex !== nextState.currentViewIndex) {
       return true
     }
@@ -18,17 +42,53 @@ class Overlay extends Component {
     }
     return true
   }
+  handleConfig = e => {
+    const target = e.target
+    const config = { ...this.state.config }
+    let key = target.name,
+      value = target.value
+
+    // Why aren't HTML elements more consistent? 😦
+    if (target.type === 'checkbox') {
+      value = target.checked
+    }
+
+    // update the value in our copied state...
+    config[key] = value
+    // ...and set it to component' state
+    this.setState({ config })
+
+    // And then save it to localStorage!
+    localStorage.setItem('horizoverlay', JSON.stringify(config))
+  }
+  toggleConfig = e => {
+    const isOpen = this.state.openConfig
+    this.setState({ openConfig: !isOpen })
+  }
   render() {
-    return (
-      <div className={`damage-meter${this.props.isActive ? '' : ' inactive'}`}>
-        <h3>Awaiting data.</h3>
-        <Encounter {...this.props.Encounter} />
-        <Combatants
-          data={this.props.Combatant}
-          encounterDamage={this.props.Encounter.damage}
+    if (this.state.openConfig) {
+      return (
+        <Config
+          toggleConfig={this.toggleConfig}
+          config={this.state.config}
+          handleConfig={this.handleConfig}
         />
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div
+          className={`damage-meter${this.props.isActive ? '' : ' inactive'}`}
+          onContextMenu={this.toggleConfig}
+        >
+          <h3>Awaiting data.</h3>
+          <Encounter {...this.props.Encounter} />
+          <Combatants
+            data={this.props.Combatant}
+            encounterDamage={this.props.Encounter.damage}
+          />
+        </div>
+      )
+    }
   }
 }
 
