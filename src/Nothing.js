3a9529cb07f4275b3ed0a099a3743b2a0cda6654
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-12 15:34:53 -0300

    Much smarter inactive window but can be better

--- src/Nothing.js
+++ src/Nothing.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import { defaultConfig } from './helpers'
+import { defaultConfig, mockData } from './helpers'
 
 import './reboot.css'
 import './nothing.css'
 
+var images = require.context('./images', false, /\.png$/)
+
 class Nothing extends Component {
+  static defaultProps = { mockData }
   state = {
     config: {}
   }
@@ -23,7 +26,7 @@ class Nothing extends Component {
   }
   openConfig = () => {
     const windowFeatures =
-      'menubar=no,location=no,resizable=no,scrollbars=yes,status=no,width=960,height=156'
+      'menubar=no,location=no,resizable=no,scrollbars=yes,status=no,width=1000,height=187'
     this.configWindow = window.open(
       '/config/',
       'Horizoverlay Config',
@@ -32,127 +35,99 @@ class Nothing extends Component {
   }
   render() {
     const colorClass = this.state.config.color
+    const isVisible = this.state.config.showSetup ? 'show' : 'hide'
     return (
       <div className={`nothing ${colorClass}`} onContextMenu={this.openConfig}>
-        <div className="combatants">
-          <div className="row Nin self job-dps" style={{ order: 1 }}>
-            <div className="name">
-              <span className="rank">1. </span>
-              <span className="character-name">Zidane Tribal (YOU)</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Nin</span>
-                <span className="damage-stats">5450.30 DPS</span>
+        <div className={`wrapper ${isVisible}`}>
+          <div className="combatants">
+            {mockData.map(mock =>
+              <div
+                className={`row${mock.isSelf ? ' self' : ''} ${this.state.config
+                  .color === 'byRole'
+                  ? mock.jobRole
+                  : ''} ${mock.jobClass} `}
+                style={{ order: mock.rank }}
+                key={mock.rank}
+              >
+                <div className="name">
+                  {this.state.config.showRank
+                    ? <span className="rank">{`${mock.rank}. `}</span>
+                    : null}
+                  <span className="character-name">
+                    {mock.isSelf ? this.state.config.characterName : mock.name}
+                  </span>
+                </div>
+                <div className="horiz-elems">
+                  {this.state.config.showJobIcon
+                    ? <img
+                        src={images(`./${mock.job}.png`)}
+                        className="job"
+                        alt={mock.jobFull}
+                      />
+                    : null}
+                  <div
+                    className={`dps${mock.isHealing
+                      ? ' relevant'
+                      : ' irrelevant'}`}
+                  >
+                    <div>
+                      <span className="damage-stats">
+                        {this.state.config.showHps
+                          ? mock.hps
+                          : mock.job.toUpperCase()}
+                      </span>
+                      <span className="label">
+                        {this.state.config.showHps ? 'HPS' : null}
+                      </span>
+                    </div>
+                  </div>
+                  <div
+                    className={`dps${mock.isHealing
+                      ? ' irrelevant'
+                      : ' relevant'}`}
+                  >
+                    <div>
+                      <span className="damage-stats">
+                        {mock.dps}
+                      </span>
+                      <span className="label">DPS</span>
+                    </div>
+                  </div>
+                </div>
+                {this.state.config.showDamagePercent
+                  ? <div>
+                      <div className="damage-percent-bg">
+                        <div
+                          className="damage-percent-fg"
+                          style={{ width: `${mock.damagePct}px` }}
+                        />
+                      </div>
+                      <div className="damage-percent">
+                        {mock.damagePct}%
+                      </div>
+                    </div>
+                  : null}
               </div>
-            </div>
+            )}
           </div>
-          <div className="row Blm job-dps" style={{ order: 2 }}>
-            <div className="name">
-              <span className="rank">2. </span>
-              <span className="character-name">Vivi Ornitier</span>
+          <div className="instructions">
+            <div>
+              <strong>Right click</strong> anywhere this window to open
+              settings!
             </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Blm</span>
-                <span className="damage-stats">5283.29 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row Smn job-dps" style={{ order: 3 }}>
-            <div className="name">
-              <span className="rank">3. </span>
-              <span className="character-name">Garnet Alexandros</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Smn</span>
-                <span className="damage-stats">2648.91 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row Drg job-dps" style={{ order: 4 }}>
-            <div className="name">
-              <span className="rank">4. </span>
-              <span className="character-name">Freya Crescent</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Drg</span>
-                <span className="damage-stats">1977.42 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row Mnk job-dps" style={{ order: 5 }}>
-            <div className="name">
-              <span className="rank">5. </span>
-              <span className="character-name">Amarant Coral</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Mnk</span>
-                <span className="damage-stats">1654.90 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row pld job-tank" style={{ order: 6 }}>
-            <div className="name">
-              <span className="rank">6. </span>
-              <span className="character-name">Adelbert Steiner</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Pld</span>
-                <span className="damage-stats">1548.24 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row War job-tank" style={{ order: 8 }}>
-            <div className="name">
-              <span className="rank">7. </span>
-              <span className="character-name">Quina Quen</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">War</span>
-                <span className="damage-stats">1366.61 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row Whm job-healer" style={{ order: 7 }}>
-            <div className="name">
-              <span className="rank">8. </span>
-              <span className="character-name">Eiko Carol</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Whm</span>
-                <span className="damage-stats">1461.64 DPS</span>
-              </div>
-            </div>
-          </div>
-          <div className="row Sch job-healer" style={{ order: 9 }}>
-            <div className="name">
-              <span className="rank">9. </span>
-              <span className="character-name">Alphinaud Cheater</span>
-            </div>
-            <div className="dps">
-              <div>
-                <span className="character-job">Sch</span>
-                <span className="damage-stats">447.18 DPS</span>
-              </div>
+            <div>
+              This is NOT real data, this is just a mock so you can place and
+              setup this overlay the way you want. Go hit a dummy or involve in
+              real combat to get real data here. <br />Also, please resize this
+              window to something like 70% your screen width.
             </div>
           </div>
         </div>
-        <div className="instructions">
+        <div className={`notice`}>
+          <span>H O R I Z O V E R L A Y</span>
           <div>
-            <strong>Right click</strong> anywhere this window to open settings!
-          </div>
-          <div>
-            This is NOT real data, this is just a mock so you can place and
-            setup this overlay the way you want. Go hit a dummy or involve in
-            real combat to get real data here. <br />Also, please resize this
-            window to something like 70% your screen width.
+            Right click here to show Settings. Be sure "Enable clickthru" in ACT
+            is off.
           </div>
         </div>
       </div>
