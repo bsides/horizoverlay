addd3fb105228bdccc2936bfc508a14f5179a248
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-12 11:40:59 -0300

    Fixed so many encounter bugs

--- src/Encounter.js
+++ src/Encounter.js
@@ -3,28 +3,24 @@ import React, { Component } from 'react'
 import './encounter.css'
 
 class Encounter extends Component {
-  shouldComponentUpdate() {
-    return this.props.title !== 'Encounter'
-  }
   render() {
     let dps =
       this.props.encdps.length <= 7 ? this.props.encdps : this.props.ENCDPS
     let rdps = parseFloat(dps)
-    // let rdps_max = 0
+    // Looks stupid but it's better than isNaN()
+    rdps = rdps !== rdps ? '∞' : rdps
 
-    // if (!isNaN(rdps) && rdps != Infinity) {
-    //   rdps_max = Math.max(rdps_max, rdps)
-    // }
-    // console.log(rdps)
-
-    // let width = rdps / rdps_max * 100
     const { config } = this.props
+    let title =
+      this.props.title === 'Encounter'
+        ? this.props.CurrentZoneName
+        : this.props.title
     let hasOptions = config.encounterTotalDps || config.encounterDuration
     return (
       <div className={`encounter${hasOptions ? ' show' : ''}`}>
         <div>
           <div className="encounter-title">
-            {this.props.title}
+            {title}
           </div>
           <div
             className={`encounter-totaldps${config.encounterTotalDps
