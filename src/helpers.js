1ec0df07f5fd5541f5dfd77207a671841df4467a
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-12 15:34:13 -0300

    Mock data for setup mode

--- src/helpers.js
+++ src/helpers.js
@@ -5,11 +5,11 @@ export function getRandom(min, max) {
 }
 
 export const defaultConfig = {
-  showSetup: true,
+  showSetup: false,
   color: 'byRole',
   characterName: 'YOU',
-  encounterDuration: true,
-  encounterTotalDps: true,
+  showDuration: true,
+  showTotalDps: true,
   showHps: true,
   showJobIcon: true,
   showRank: true,
@@ -35,4 +35,124 @@ export const jobsDps = [
   'sam',
   'smn',
   'thm'
+]
+
+export const mockData = [
+  {
+    isSelf: true,
+    name: 'YOU',
+    jobClass: 'Nin',
+    jobFull: 'Ninja',
+    job: 'nin',
+    jobRole: 'job-dps',
+    rank: 1,
+    dps: '5450.30',
+    hps: '0.0',
+    isHealing: false,
+    damagePct: '36'
+  },
+  {
+    isSelf: false,
+    name: 'Vivi Ornitier',
+    jobClass: 'Blm',
+    jobFull: 'Black Mage',
+    job: 'blm',
+    jobRole: 'job-dps',
+    rank: 2,
+    dps: '5283.29',
+    hps: '0.0',
+    isHealing: false,
+    damagePct: '32'
+  },
+  {
+    isSelf: false,
+    name: 'Zidane Tribal',
+    jobClass: 'Nin',
+    jobFull: 'Ninja',
+    job: 'nin',
+    jobRole: 'job-dps',
+    rank: 3,
+    dps: '2648.91',
+    hps: '4.12',
+    isHealing: false,
+    damagePct: '22'
+  },
+  {
+    isSelf: false,
+    name: 'Garnet Alexandros',
+    jobClass: 'Smn',
+    jobFull: 'Summoner',
+    job: 'smn',
+    jobRole: 'job-dps',
+    rank: 4,
+    dps: '1654.90',
+    hps: '12.10',
+    isHealing: false,
+    damagePct: '15'
+  },
+  {
+    isSelf: false,
+    name: 'Freya Crescent',
+    jobClass: 'Drg',
+    jobFull: 'Dragoon',
+    job: 'drg',
+    jobRole: 'job-dps',
+    rank: 5,
+    dps: '1977.42',
+    hps: '3.10',
+    isHealing: false,
+    damagePct: '16'
+  },
+  {
+    isSelf: false,
+    name: 'Adelbert Steiner',
+    jobClass: 'Pld',
+    jobFull: 'Paladin',
+    job: 'pld',
+    jobRole: 'job-tank',
+    rank: 6,
+    dps: '1548.24',
+    hps: '588.50',
+    isHealing: false,
+    damagePct: '9'
+  },
+  {
+    isSelf: false,
+    name: 'Quina Quen',
+    jobClass: 'War',
+    jobFull: 'Warrior',
+    job: 'war',
+    jobRole: 'job-tank',
+    rank: 8,
+    dps: '1366.61',
+    hps: '112.50',
+    isHealing: false,
+    damagePct: '6'
+  },
+  {
+    isSelf: false,
+    name: 'Eiko Carol',
+    jobClass: 'Whm',
+    jobFull: 'White Mage',
+    job: 'whm',
+    jobRole: 'job-healer',
+    rank: 7,
+    dps: '1461.64',
+    hps: '9821.50',
+    isHealing: true,
+    damagePct: '7'
+  },
+  {
+    isSelf: false,
+    name: 'Alphinaud Cheater',
+    jobClass: 'Sch',
+    jobFull: 'Scholar',
+    job: 'sch',
+    jobRole: 'job-healer',
+    rank: 9,
+    dps: '447.18',
+    hps: '5661.12',
+    isHealing: true,
+    damagePct: '2'
+  }
 ]
