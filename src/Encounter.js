086971606b981d3af64906539b8f8f6406f63cfd
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-17 10:40:51 -0300

    Abusing of && syntax

--- src/Encounter.js
+++ src/Encounter.js
@@ -4,38 +4,34 @@ import './css/encounter.css'
 
 class Encounter extends Component {
   render() {
+    const { config } = this.props
     let dps =
       this.props.encdps.length <= 7 ? this.props.encdps : this.props.ENCDPS
-    let rdps = parseFloat(dps)
+    let totalDps = parseFloat(dps)
     // Looks stupid but it's better than isNaN()
     // eslint-disable-next-line
-    rdps = rdps !== rdps ? '∞' : rdps
+    totalDps = totalDps !== totalDps ? '∞' : totalDps
 
-    const { config } = this.props
     let title =
       this.props.title === 'Encounter'
         ? this.props.CurrentZoneName
         : this.props.title
     let hasOptions = config.showTotalDps || config.showDuration
     return (
-      <div className={`encounter${hasOptions ? ' show' : ''}`}>
+      <div className={`encounter${hasOptions && ' show'}`}>
         <div>
           <div className="encounter-title">
             {title}
           </div>
           <div
-            className={`encounter-totaldps${config.showTotalDps
-              ? ' show'
-              : ''}`}
+            className={`encounter-totaldps${config.showTotalDps && ' show'}`}
           >
-            {rdps} DPS
+            {totalDps} DPS
           </div>
           <div
-            className={`encounter-duration${config.showDuration
-              ? ' show'
-              : ''}`}
+            className={`encounter-duration${config.showDuration && ' show'}`}
           >
-            <span role="img" aria-label="Clock">
+            <span role="img" aria-label="Time">
               🕒
             </span>{' '}
             {this.props.duration}
