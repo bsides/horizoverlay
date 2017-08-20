90ee8c4f2a0ac9acea254399b7400fcbb2dacff1
Rafael Pereira <rafaelbsides@gmail.com>
2017-08-20 00:02:21 -0300

    Commenting later implementation

--- src/index.js
+++ src/index.js
@@ -44,23 +44,24 @@ const Root = detail => {
 // This will run when data is ON
 function onOverlayDataUpdate(e) {
   // discordString is true whenever the user uses '/e discord' in game
-  const discordString =
-    'detail' in e &&
-    'payload' in e.detail &&
-    e.detail.payload[2].toLowerCase().indexOf('discord')
+  // const discordString =
+  //   'detail' in e &&
+  //   'payload' in e.detail &&
+  //   e.detail.payload[2].toLowerCase().indexOf('discord')
 
   // regardless, we keep sending combat data
-  if (e.type === 'onOverlayDataUpdate' || discordString) {
-    // ... but we save the last data in case the next data isn't combat data
-    window.lastData = e.detail
-    ReactDOM.render(<Root {...e.detail} />, document.getElementById('root'))
-  } else if (discordString && window.lastData !== {}) {
-    // then we send the last data here, which won't update combat numbers but will send discord stuff
-    ReactDOM.render(
-      <Root detail={window.lastData} discord={true} />,
-      document.getElementById('root')
-    )
-  }
+  // if (e.type === 'onOverlayDataUpdate' || discordString) {
+  // ... but we save the last data in case the next data isn't combat data
+  //   window.lastData = e.detail
+  //   ReactDOM.render(<Root {...e.detail} />, document.getElementById('root'))
+  // } else if (discordString && window.lastData !== {}) {
+  // then we send the last data here, which won't update combat numbers but will send discord stuff
+  // ReactDOM.render(
+  //   <Root detail={window.lastData} discord={true} />,
+  //   document.getElementById('root')
+  // )
+  // }
+  ReactDOM.render(<Root {...e.detail} />, document.getElementById('root'))
 }
 // This will run when there's no data
 ReactDOM.render(<Inactive />, document.getElementById('root'))
@@ -72,9 +73,12 @@ ReactDOM.render(<Inactive />, document.getElementById('root'))
 // - onOverlayDataUpdate
 // This event occurs when the OverlayPlugin sends the new data.
 document.addEventListener('onOverlayDataUpdate', onOverlayDataUpdate)
+
 // - onLogLine
 // This event occurs when anything in the chat happens, so we need to clean it up a bit before sending to the component or else it will polute and re-render it a lot unnecessarily
-document.addEventListener('onLogLine', onOverlayDataUpdate)
+// Not being implemented right now
+// document.addEventListener('onLogLine', onOverlayDataUpdate)
+
 // - onOverlayStateUpdate
 // This event occurs when the overlay setting has changed.
 document.addEventListener('onOverlayStateUpdate', function(e) {
