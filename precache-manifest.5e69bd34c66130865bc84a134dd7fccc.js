581f535ec4267f712de52884032097482094ce98
Rafael @bsides Pereira <rafaelbsides@gmail.com>
2019-07-21 04:30:41 -0300

    Updates

---
+++ precache-manifest.5e69bd34c66130865bc84a134dd7fccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c6e110ba9ea6af413cec186138a0e9",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "4f624083729e8e34193d",
+    "url": "/horizoverlay/static/css/main.b0a6984b.chunk.css"
+  },
+  {
+    "revision": "7a2f89d10b511b9c0d33",
+    "url": "/horizoverlay/static/js/2.b7868ab6.chunk.js"
+  },
+  {
+    "revision": "4f624083729e8e34193d",
+    "url": "/horizoverlay/static/js/main.686de8fc.chunk.js"
+  },
+  {
+    "revision": "f5cff2eb4effd23c272c",
+    "url": "/horizoverlay/static/js/runtime~main.5766f4f7.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  }
+]);
