6f22ae9e826507abc59cf9ad6a0d6ff4e258bcd8
Jacob Godserv <jacobgodserv@gmail.com>
2020-06-13 00:36:28 -0400

    Default to combining pets

--- src/helpers.js
+++ src/helpers.js
@@ -21,7 +21,7 @@ export const defaultConfig = {
   discord: '',
   maxCombatants: 8,
   locale: 'enUS',
-  combinePets: false,
+  combinePets: true,
   configWindow: {
     width: 1300,
     height: 239
