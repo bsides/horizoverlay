4e549726e38d2c57562fb828af235428e22170a4
Eric <github.kuphar@aleeas.com>
2021-12-04 18:09:03 -0800

    Prevent error when missing job icons to fix #142

--- src/CombatantHorizontal.js
+++ src/CombatantHorizontal.js
@@ -71,7 +71,12 @@ export default class CombatantHorizontal extends Component {
       } else {
         jobIcon += data.Job.toLowerCase()
       }
-      jobIcon = images(`${jobIcon}.png`)
+      try {
+        jobIcon = images(`${jobIcon}.png`)
+      } catch (e) {
+        console.error(e)
+        jobIcon = images('./empty.png')
+      }
     }
 
     // Character name (self, instead of 'YOU')
